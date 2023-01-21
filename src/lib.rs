365907c826fd028f608b2d8e3fa524e777b803ff
gc2004 <55647897+undersquire@users.noreply.github.com>
2023-01-21 09:34:05 -0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -95,12 +95,6 @@ mod tests {
     fn test_hmac() {
         let mac = Hmac::<64, 16, Md5>::mac(b"Hello, world!", b"test");
 
-        if cfg!(target_endian = "big") {
-            println!("Big Endian");
-        } else {
-            println!("Little Endian");
-        }
-
         assert_eq!(
             mac.encode_hex::<String>(),
             "5286559abc8deaa260e8335499b2f6da"
